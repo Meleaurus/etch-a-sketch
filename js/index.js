363812270c856c0ad7fdf38ec842fafddb367fcb
Melvin Teo <mthteo@gmail.com>
2020-10-10 11:49:40 -0700

    figured out how to change colors when changing grid size, now finding random color function

--- js/index.js
+++ js/index.js
@@ -1,5 +1,26 @@
 const container = document.querySelector("#container");
-const btn = document.querySelector("#btn");
+const clearBtn = document.querySelector("#clearBtn");
+const randomBtn = document.querySelector("#randomBtn");
+
+addHover = (square) => {
+    square.addEventListener("mouseover", function(event) {
+        event.target.classList.replace("square", "hover");
+    });
+}
+
+getSquare = () => {
+    let square = document.getElementsByClassName('square');
+    let squareArr = Array.from(square);
+    squareArr.forEach(addHover);
+}
+
+randomColor = () => {
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    return bgColor;
+}
 
 createGrid = () => {
     for (let i = 0; i < 256; i++) {
@@ -7,40 +28,43 @@ createGrid = () => {
         div.classList.add('square'); 
         container.appendChild(div);
     }
+    getSquare();
 }
 createGrid();
 
-btn.addEventListener('click', function() {
+randomBtn.addEventListener('click', function() {
+    let square = document.getElementsByClassName('square');
+    let squareArr = Array.from(square);
+    squareArr.forEach(randomColor);
+})
+
+clearBtn.addEventListener('click', function() {
     updateGrid();
 })
 
 updateGrid = () => {
     container.innerHTML = "";
-    let size = parseInt(prompt('Please enter in a value for the grid side length: ', 16));
-    for (let i = 0; i < size*size; i++) {
-        const div = document.createElement('div');
-        div.classList.add('square');
-        container.appendChild(div);
+    let size = parseInt(prompt('Please enter in a value between 10 and 100: ', 16));
+    if (size <= 100 && size >= 10) {
+        for (let i = 0; i < size*size; i++) {
+            const div = document.createElement('div');
+            div.classList.add('square');
+            container.appendChild(div);
+        }
+        container.style.setProperty(
+            "grid-template-columns",
+            `repeat(${size}, 1fr)`
+        );
+        container.style.setProperty(
+            "grid-template-rows",
+            `repeat(${size}, 1fr)`
+        );
+        getSquare();
     }
-    container.style.setProperty(
-        "grid-template-columns",
-        `repeat(${size}, 1fr)`
-    );
-    container.style.setProperty(
-        "grid-template-rows",
-        `repeat(${size}, 1fr)`
-    );
 }
 
-function addEvent(square) {
-    square.addEventListener("mouseover", function(event) {
-        event.target.classList.replace("square", "hover");
-    });
-}
 
-let square = document.getElementsByClassName('square');
-let squareArr = Array.from(square);
-squareArr.forEach(addEvent);
+
 
 
     
