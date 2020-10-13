a17e491d5045089e6ce9ae32e77c68b133b7303b
Melvin Teo <mthteo@gmail.com>
2020-10-12 22:24:46 -0700

    integrated color radomizer, working on the darken aspect still

--- js/index.js
+++ js/index.js
@@ -1,10 +1,25 @@
 const container = document.querySelector("#container");
-const clearBtn = document.querySelector("#clearBtn");
+const resizeBtn = document.querySelector("#resizeBtn");
 const randomBtn = document.querySelector("#randomBtn");
+const darkenBtn = document.querySelector('#darkenBtn')
+const resetBtn = document.querySelector("#resetBtn");
 
 addHover = (square) => {
     square.addEventListener("mouseover", function(event) {
-        event.target.classList.replace("square", "hover");
+        event.target.classList.add("hover");
+    });
+}
+
+addHoverRandom = (square) => {
+    square.addEventListener("mouseover", function(event) {
+        square.style.backgroundColor = randomColor();
+    });
+}
+
+addHoverDarken = (square) => {
+    square.addEventListener("mouseover", function(event) {
+        square.style.backgroundColor = "black";
+        square.style.opacity = String(Math.random());
     });
 }
 
@@ -18,8 +33,8 @@ randomColor = () => {
     let x = Math.floor(Math.random() * 256);
     let y = Math.floor(Math.random() * 256);
     let z = Math.floor(Math.random() * 256);
-    let bgColor = "rgb(" + x + "," + y + "," + z + ")";
-    return bgColor;
+    let color = "rgb(" + x + "," + y + "," + z + ")";
+    return color;
 }
 
 createGrid = () => {
@@ -28,38 +43,70 @@ createGrid = () => {
         div.classList.add('square'); 
         container.appendChild(div);
     }
+    container.style.setProperty(
+        "grid-template-columns",
+        `repeat(16, 1fr)`
+    );
+    container.style.setProperty(
+        "grid-template-rows",
+        `repeat(16, 1fr)`
+    );
     getSquare();
 }
 createGrid();
 
-randomBtn.addEventListener('click', function() {
-    let square = document.getElementsByClassName('square');
-    let squareArr = Array.from(square);
-    squareArr.forEach(randomColor);
+resetBtn.addEventListener('click', function() {
+    container.innerHTML = "";
+    createGrid();
 })
 
-clearBtn.addEventListener('click', function() {
+randomBtn.addEventListener('click', function() {
+    squares = document.querySelectorAll(".square");
+    squares.forEach(addHoverRandom);
+})
+
+darkenBtn.addEventListener('click', function() {
+    squares = document.querySelectorAll(".square");
+    squares.forEach(addHoverDarken);
+})
+
+resizeBtn.addEventListener('click', function() {
     updateGrid();
 })
 
 updateGrid = () => {
     container.innerHTML = "";
     let size = parseInt(prompt('Please enter in a value between 10 and 100: ', 16));
-    if (size <= 100 && size >= 10) {
-        for (let i = 0; i < size*size; i++) {
-            const div = document.createElement('div');
-            div.classList.add('square');
-            container.appendChild(div);
+    try {
+       if(size == "") throw "empty";
+       if(isNaN(size)) throw "not a number";
+       size = Number(size);
+       if(size < 10) throw "too low";
+       if(size > 100) throw "too high";
+    }
+    catch(err) {
+        alert(`Input is ${err}`);
+    }
+    finally {
+        if (size <= 100 && size >= 10) {
+            for (let i = 0; i < size*size; i++) {
+                const div = document.createElement('div');
+                div.classList.add('square');
+                container.appendChild(div);
+            }
+            container.style.setProperty(
+                "grid-template-columns",
+                `repeat(${size}, 1fr)`
+            );
+            container.style.setProperty(
+                "grid-template-rows",
+                `repeat(${size}, 1fr)`
+            );
+            getSquare();
         }
-        container.style.setProperty(
-            "grid-template-columns",
-            `repeat(${size}, 1fr)`
-        );
-        container.style.setProperty(
-            "grid-template-rows",
-            `repeat(${size}, 1fr)`
-        );
-        getSquare();
+        else {
+            createGrid();
+        }
     }
 }
 
